e6314579ffd863eaa89f7fee1b95b13d6e80c0df
nobuhiro.kurusu <kurusu114@gmail.com>
2020-04-07 16:19:33 +0600

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 來栖暢宏 on 07.04.2020.
+//  Copyright © 2020 nobuhiro.kurusu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
